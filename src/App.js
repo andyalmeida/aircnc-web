e6261dc9b63f528f79719dda1bcabd011a21d863
André Almeida <a.alves.de.almeida@avanade.com>
2019-10-03 18:23:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="Aircnc"/>
+      <div className="content">
+        <p>Ofereça <strong>sports</strong> para progrmadores e encontre <strong>talentos</strong> para sua empresa</p>
+
+        <form action="">
+          <label htmlFor="email">EMAIL *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn">Entrar</button>
+          </form>
+      </div>
     </div>
   );
 }
