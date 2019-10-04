5b2a9aa9d963a96d5322d44fe604df2105c1b389
André Almeida <a.alves.de.almeida@avanade.com>
2019-10-04 18:43:14 -0300

    finalização do frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import Routes from './routes';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+
+
   return (
     <div className="container">
       <img src={logo} alt="Aircnc"/>
       <div className="content">
-        <p>Ofereça <strong>sports</strong> para progrmadores e encontre <strong>talentos</strong> para sua empresa</p>
-
-        <form action="">
-          <label htmlFor="email">EMAIL *</label>
-          <input 
-            id="email" 
-            type="email" 
-            placeholder="Seu melhor e-mail"
-          />
-          <button type="submit" className="btn">Entrar</button>
-          </form>
+        <Routes />
       </div>
     </div>
   );
